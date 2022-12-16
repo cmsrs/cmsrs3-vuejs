be2ec56c902e11346117559bfa336e1e2b699ef5
Robert <robert.szczepanski@cmsrs.pl>
2022-12-16 14:12:59 +0100

    login page

--- src/pages/LoginPage.spec.js
+++ src/pages/LoginPage.spec.js
@@ -2,8 +2,59 @@ import LoginPage from "./LoginPage.vue";
 import {render, screen} from "@testing-library/vue";
 
 
-it( 'has login header', ()  => {
+const setup = async () => {
     render(LoginPage);
-    const header = screen.queryByRole("heading", { name: "Login" });
-    expect(header).toBeInTheDocument();
+    /*
+    render(LoginPage, {
+      global: {
+        plugins: [i18n, store],
+        mocks: {
+          $router: {
+            push: () => {},
+          },
+        },
+      },
+    });
+    emailInput = screen.queryByLabelText("E-mail");
+    passwordInput = screen.queryByLabelText("Password");
+    button = screen.queryByRole("button", { name: "Login" });
+    */
+  };
+
+
+describe("Login page", () => {
+    describe("Layout", () => {
+        it( 'has login header', async ()  => {
+            await setup();
+            const header = screen.queryByRole("heading", { name: "Login" });
+            expect(header).toBeInTheDocument();
+        });
+        
+        it("has email input", async () => {
+            await setup();
+            const input = screen.queryByLabelText("E-mail");
+            expect(input).toBeInTheDocument();
+        });
+        it("has password input", async () => {
+          await setup();
+          const input = screen.queryByLabelText("Password");
+          expect(input).toBeInTheDocument();
+        });
+        it("has password type for password input", async () => {
+          await setup();
+          const input = screen.queryByLabelText("Password");
+          expect(input.type).toBe("password");
+        });
+        it("has Login button", async () => {
+          await setup();
+          const button = screen.queryByRole("button", { name: "Login" });
+          expect(button).toBeInTheDocument();
+        });
+        it("disables the button initially", async () => {
+          await setup();
+          const button = screen.queryByRole("button", { name: "Login" });
+          expect(button).toBeDisabled();
+        });
+        
+    });
 });
--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,9 +1,81 @@
 <template>
-    <h1>Login</h1>    
+    <h1>Login</h1>  
+    <Input id="e-mail" label="E-mail" v-model="email" />
+    <Input
+    id="password"
+    label="Password"
+    v-model="password"
+    type="password"
+    />
+    <div class="alert alert-danger text-center" v-if="failMessage">
+            {{ failMessage }}
+    </div>
+    <div class="text-center">
+    <ButtonWithProgress
+        :apiProgress="apiProgress"
+        :disabled="isDisabled"
+        :onClick="submit"
+    >
+        Login
+    </ButtonWithProgress>
+    </div>
+
 </template>
 <script>
+import Input from "../components/Input";
+import ButtonWithProgress from "../components/ButtonWithProgress";
 export default {
     name: "LoginPage",
+    components: {
+        Input,
+        ButtonWithProgress
+    },
+    data() {
+        return {
+        email: "",
+        password: "",
+        apiProgress: false,
+        failMessage: undefined,
+        };
+    },
+    computed: {
+        isDisabled() {
+            return !(this.email && this.password);
+        },
+    },
+    methods: {
+        async submit() {
+        this.apiProgress = true;
+        try {
+            console.log('________submit____');
+            /*
+            const response = await login({
+            email: this.email,
+            password: this.password,
+            });
+            this.$router.push("/");
+
+            const data = {
+            ...response.data,
+            header: `Bearer ${response.data.token}`,
+            };
+
+            this.$store.commit("loginSuccess", data);
+            */
+        } catch (error) {
+            this.failMessage = error.response.data.message;
+        }
+        this.apiProgress = false;
+        },
+    },
+    watch: {
+        email() {
+            this.failMessage = undefined;
+        },
+        password() {
+            this.failMessage = undefined;
+        },
+    }
 }
 </script>
 
