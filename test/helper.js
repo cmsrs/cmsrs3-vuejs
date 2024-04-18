557d314ed9f507655b50c907921ea81937af60bd
Robert <robert.szczepanski@cmsrs.pl>
2024-04-18 14:09:02 +0200

    remove unsed variable

--- test/helper.js
+++ test/helper.js
@@ -1,10 +1,10 @@
 import router from '../src/router/index.js'
-import userEvent from '@testing-library/user-event'
+//import userEvent from '@testing-library/user-event'
 import { render } from '@testing-library/vue'
 import { createPinia } from 'pinia'
 
 const customRender = (component, options) => {
-  const user = userEvent.setup()
+  //const user = userEvent.setup()
   const result = render(component, {
     global: {
       plugins: [router, createPinia()]
@@ -13,7 +13,7 @@ const customRender = (component, options) => {
   })
   return {
     result,
-    user
+    //user
   }
 }
 
