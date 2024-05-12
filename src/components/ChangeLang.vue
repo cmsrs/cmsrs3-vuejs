2e6701fa70434c7b4889416e2441e4f1def832ba
Robert <robert.szczepanski@cmsrs.pl>
2024-05-12 13:40:23 +0200

    fix change lang

--- src/components/ChangeLang.vue
+++ src/components/ChangeLang.vue
@@ -1,15 +1,18 @@
 <template>
         <div role="change_lang" class="col-2" v-if="langs.length > 1">
-          <div v-for="l in langs" :key="l">
-            <span
-              :role="'lang_' + l"
-              :class="{
-                'me-1 cursor-pointer text-primary': lang === l,
-                'me-1 cursor-pointer text-secondary': lang !== l,
-              }"
-              @click="$emit('execChangeLang', l)"              
-              >{{ l }}
-            </span>
+          <div class="row">
+            <div v-for="l in langs" :key="l"  class="col-1">
+              <span
+                :role="'lang_' + l"
+                class="lang"
+                :class="{
+                  'me-1 cursor-pointer text-primary': lang === l,
+                  'me-1 cursor-pointer text-secondary': lang !== l,
+                }"
+                @click="$emit('execChangeLang', l)"              
+                >{{ l }}
+              </span>
+            </div>
           </div>
         </div>
 </template>
