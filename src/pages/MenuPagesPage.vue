ad081d1d450c23039bdc64957b7f79f63d836c39
Robert <robert.szczepanski@cmsrs.pl>
2024-05-20 14:30:47 +0200

    fix after manual testing

--- src/pages/MenuPagesPage.vue
+++ src/pages/MenuPagesPage.vue
@@ -1023,6 +1023,8 @@ const getImagesByCurrentId = async () => {
 };
 
 const resetSelectedItems = async () => {
+  selectedAllItems.value = false;
+  
   if (!currentPageId.value) {
     return false;
   }
