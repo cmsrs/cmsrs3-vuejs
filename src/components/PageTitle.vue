d187a1435f91e5f752a90f7b3608b17e03e504f2
Robert <robert.szczepanski@cmsrs.pl>
2024-07-13 18:00:38 +0200

    click p exactly page title

--- src/components/PageTitle.vue
+++ src/components/PageTitle.vue
@@ -33,18 +33,19 @@
       >
         <i class="fas fa-arrow-up cursor-pointer" aria-hidden="true"></i>
       </div>
-      <span
-        class="col"
-        :class="{ 'text-secondary': !p.published }"
-        @click="$emit('execEditPage', p.id)"
-      >
+      <span class="col" :class="{ 'text-secondary': !p.published }">
         <span
+          @click="$emit('execEditPage', p.id)"
           v-if="showPageId"
           :class="{ 'text-primary': p.id == currentPageId }"
         >
           {{ p.short_title[lang] + " (" + p.id + ")" }}
         </span>
-        <span v-else :class="{ 'text-primary': p.id == currentPageId }">
+        <span
+          v-else
+          :class="{ 'text-primary': p.id == currentPageId }"
+          @click="$emit('execEditPage', p.id)"
+        >
           {{ p.short_title[lang] }}
         </span>
       </span>
